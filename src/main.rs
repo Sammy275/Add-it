ee2a6614886d00b1930a117c0b2dfb8e0f6370cb
Saim Irfan <samfan95@gmail.com>
2020-06-25 17:06:07 +0500

    added comments

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use rocket::response::content::Html;
 
 #[get("/")]
-fn index() -> Html<&'static str> {
+fn index() -> Html<&'static str> { //Returning an html page
     Html(r#"<!DOCTYPE html>
 <html>
     <head>
@@ -23,7 +23,7 @@ fn index() -> Html<&'static str> {
 
 #[get("/addit/<number>")]
 fn addit(number: i32) -> String {
-    let new_number = number + 100;
+    let new_number = number + 100;          //100 is being added into the requested number
     format!("You entered {}, We added 100, new number is {}", number, new_number)
 }
 
@@ -31,5 +31,10 @@ fn addit(number: i32) -> String {
 fn main() {
     rocket::ignite()
         .mount("/", routes![index, addit]) // Mounting the handlers
-        .launch();
+        .launch();  //launching the server
 }
+
+
+//Written by: Saim Irfan IOT049609
+// will be updated for the purpose of practise
+// Pakistan Zindabad
