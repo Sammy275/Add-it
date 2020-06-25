135d16ef3c8f62840deab81a80d1eafdb649560a
Saim Irfan <samfan95@gmail.com>
2020-06-25 17:40:44 +0500

    added route for making an html POST request

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@
 #[macro_use] extern crate rocket;
 
 use rocket::response::content::Html;
+use rocket::request::Form;
+
+
+#[derive(FromForm)]     //Making a struct to parse the user post request
+struct UserForm {
+    number: i32
+}
+
 
 #[get("/")]
 fn index() -> Html<&'static str> { //Returning an html page
@@ -26,10 +34,34 @@ fn addit(number: i32) -> String {
     format!("You entered {}, We added 100, new number is {}", number, new_number)
 }
 
+#[post("/addit", data="<form>")]
+fn user_post(form: Form<UserForm>) -> String {
+    let new_number = form.number + 100;
+    format!("You entered {}, We added 100, new number is {}", form.number, new_number)
+}
+
+#[get("/form")]                         //Handler to display html document for user to make a post request
+fn form() -> Html<&'static str> {
+    Html(r#"<!DOCTYPE html>
+<html>
+    <head>
+        <title>Post</title>
+    </head>
+    <body>
+        <h1>Type a number that you want to add to 100</h1>
+        <br>
+        <form method="POST" action="/addit">
+            <input type="number" placeholer="Any Number" name="number"><br><br>
+            <input type="submit" value="Send">
+        </form>
+    </body>
+</html>"#)
+}
+
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index, addit]) // Mounting the handlers
+        .mount("/", routes![index, addit, form, user_post]) // Mounting the handlers
         .launch();  //launching the server
 }
 
