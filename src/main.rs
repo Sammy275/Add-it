1283335970eee7e7d169fa89a2f0e36da0ef0e4b
Saim Irfan <samfan95@gmail.com>
2020-06-25 16:56:23 +0500

    Added a home route

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
+use rocket::response::content::Html;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, World"
+fn index() -> Html<&'static str> {
+    Html(r#"<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+    <body>
+        <h1>Welcome</h1>
+        <p>This is a rocket practise</p>
+        <p>Please Go to the '/add/{number}'</p>
+        <p>You can input any number inside the uri</p>
+    </body>
+</html>"#)
 }
 
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes![index]) // Mounting the handlers
         .launch();
 }
